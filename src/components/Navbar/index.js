286bedda48fde251d7498020c789ca186daec54c
Adriano Tadao <adrianotadao@gmail.com>
2017-11-02 15:31:52 -0200

    Building the navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return(
+      <nav>
+        <h2> Time keeper </h2>
+        <ul>
+          <li> Log out </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import Sidenav from '../../components/Sidenav'
+import Navbar from '../../components/Navbar'
 import WorksheetList from '../WorksheetList'
 
 import styles from './styles'
@@ -10,6 +11,7 @@ class Root extends Component {
   render() {
     return(
       <div className={ styles.content }>
+        <Navbar />
         <Sidenav />
 
         <main className={ styles.main }>
