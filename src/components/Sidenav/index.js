4f496c213452f62c4698c26a568441f4a5a322a7
Adriano Tadao <adrianotadao@gmail.com>
2017-11-02 12:23:51 -0200

    Added the first one style and glamor

--- src/components/Sidenav/index.js
+++ src/components/Sidenav/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import * as styles from './index.styles'
+
 class Sidenav extends Component {
   render() {
     return(
-      <nav>
+      <nav className={ styles.sidenav }>
         <ul>
           <li><Link to='/'> Home </Link></li>
           <li><Link to='/worksheets'> Worksheets </Link></li>
