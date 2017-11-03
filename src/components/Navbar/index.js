122f5a383bd01fa9d0dc81fefeda198760515a83
Adriano Tadao <adrianotadao@gmail.com>
2017-11-03 09:17:51 -0200

    Stylizing the sidebar and navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
+import styles from './styles'
+
 class Navbar extends Component {
   render() {
     return(
-      <nav>
+      <nav className={ styles.nav }>
         <h2> Time keeper </h2>
         <ul>
           <li> Log out </li>
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,7 @@
+import { css } from 'glamor'
+
+export default {
+  nav: css({
+    backgroundColor: 'red'
+  })
+}
